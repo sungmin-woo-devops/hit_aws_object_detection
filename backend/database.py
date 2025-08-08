824820d3d899737ae736260ab3d68ca2721a0bef
sungmin-woo-devops <sungminwoo.devops@gmail.com>
2025-08-08 16:16:35 +0900

    add backend 02

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,25 @@
-from supabase import create_client, Client
-from dotenv import load_dotenv
+# from supabase import create_client, Client
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from models import Base
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_SECRET_KEY = os.getenv("SUPABASE_SECRET_KEY")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://<user>:<password>@<host>:<port>/<database_name>?options=--search_path=public"
+)
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-supbase: Client = create_client(SUPABASE_URL, SUPABASE_SECRET_KEY)
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
